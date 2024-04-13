f4d88048eb812d9de2ded8a2aa3cd64e4cc9910c
Ahmad Iqbal Firmansyah <ahmedeqbal101@gmail.com>
2024-04-13 19:02:35 +0700

    Pertemuan 09 - Praktikum 4

--- Pertemuan_9/praktik_php/koneksi.php
+++ Pertemuan_9/praktik_php/koneksi.php
@@ -5,5 +5,7 @@ $password = "";
 $database = "prakwebdb";
 
 $connect = mysqli_connect($servername, $username, $password, $database);
-
+if(!$connect) {
+    die("Koneksi gagal: " . mysqli_connect_error());
+}
 ?>
