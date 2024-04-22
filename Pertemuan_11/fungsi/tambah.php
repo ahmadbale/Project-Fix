6cdada146add1911f3d99645ad3c851ea1dcc463
Ahmad Iqbal Firmansyah <ahmedeqbal101@gmail.com>
2024-04-22 10:47:16 +0700

    Pertemuan 11 - Praktikum 5

--- Pertemuan_11/fungsi/tambah.php
+++ Pertemuan_11/fungsi/tambah.php
@@ -7,17 +7,14 @@ if (!empty($_SESSION['username'])) {
     if (!empty($_GET['jabatan'])) {
         $jabatan = antiinjection($koneksi, $_POST['jabatan']);
         $keterangan = antiinjection($koneksi, $_POST['keterangan']);
-        $query = "INSERT INTO jabatan (jabatan, keterangan) VALUES('$jabatan', '$keterangan')";
+        $query = "INSERT INTO jabatan (jabatan, keterangan) VALUES ('$jabatan', '$keterangan')";
         if (mysqli_query($koneksi, $query)) {
-            pesan(
-                'success',
-                "Jabatan Baru Ditambahkan."
-            );
+            pesan('success', "Jabatan Baru Ditambahkan");
         } else {
-            pesan('danger', "Gagal Menambahkan Jabatan Karena: " . mysqli_error($koneksi));
+            pesan('danger', "Menambahkan Jabatan karena : " . mysqli_error($koneksi));
         }
         header("Location: ../index.php?page=jabatan");
-    } elseif (!empty($_GET['anggota'])) {
+    } else if (!empty($_GET['anggota'])) {
         $username = antiinjection($koneksi, $_POST['username']);
         $password = antiinjection($koneksi, $_POST['password']);
         $level = antiinjection($koneksi, $_POST['level']);
@@ -31,22 +28,22 @@ if (!empty($_SESSION['username'])) {
         $combined_password = $salt . $password;
         $hashed_password = password_hash($combined_password, PASSWORD_BCRYPT);
 
-        $query = "INSERT INTO user_2 (username, password, salt, level) VALUES ('$username', '$hashed_password', '$salt','$level')";
+        $query = "INSERT INTO user_2 (username, password, salt, level) VALUES ('$username', '$hashed_password', '$salt', '$level')";
 
         if (mysqli_query($koneksi, $query)) {
             $last_id = mysqli_insert_id($koneksi);
-            $query2 = "INSERT INTO anggota (nama, jenis_kelamin, alamat, no_telp, user_id, jabatan_id) VALUES ('$nama', '$jenis_kelamin', '$alamat', '$no_telp', '$last_id','$jabatan')";
-
+            $query2 = "INSERT INTO anggota (nama, jenis_kelamin, alamat, no_telp, user_id, jabatan_id) VALUES ('$nama', '$jenis_kelamin', '$alamat', '$no_telp', '$last_id', '$jabatan')";
             if (mysqli_query($koneksi, $query2)) {
-                pesan('success', "Anggota Baru Ditambahkan.");
+                pesan('success', "Anggota Baru Ditambahkan");
             } else {
-                pesan('warning', "Gagal Menabahkan Anggota Tetapi Data Login Tersimpan Karena: " . mysqli_error($koneksi));
+                pesan('warning', "Gagal Menambahkan Anggota Tetapi Data Login Tersimpan Karena : " . mysqli_error($koneksi));
             }
         } else {
-            pesan('danger', "Gagal Menambahkan Anggota Karena: " . mysqli_error($koneksi));
+            pesan('danger', "Gagal? Menambahkan Anggota Karena : " . mysqli_error($koneksi));
         }
         header("Location: ../index.php?page=anggota");
     }
 } else {
     header("Location: ../index.php?page=login");
 }
+?>
